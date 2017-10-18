5df981b43d86b28dce0e374973ddf87f90a7ed32
Rires_Magica <rires.magica.l@gmail.com>
2017-10-18 21:54:30 +0900

    add: IMachine.java

---
+++ src/main/java/net/rires/hmagemod/machines/IMachine.java
@@ -0,0 +1,9 @@
+package net.rires.hmagemod.machines;
+
+public interface IMachine {
+
+  double getEnergy();
+
+  boolean isRunning();
+
+}
--- src/main/java/net/rires/hmagemod/machines/block/HmageFurnaceBlock.java
+++ src/main/java/net/rires/hmagemod/machines/block/HmageFurnaceBlock.java
@@ -2,11 +2,21 @@ package net.rires.hmagemod.machines.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.rires.hmagemod.machines.IMachine;
 
-public class HmageFurnaceBlock extends Block {
+public class HmageFurnaceBlock extends Block implements IMachine {
 
   public HmageFurnaceBlock() {
     super(Material.iron);
   }
 
+  @Override
+  public double getEnergy() {
+    return 0;
+  }
+
+  @Override
+  public boolean isRunning() {
+    return false;
+  }
 }

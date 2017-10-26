897e6184b98f9288564c8230d5ecbcbb0adc2be3
Rires_Magica <rires.magica.l@gmail.com>
2017-10-26 22:38:39 +0900

    hmage

--- src/main/java/net/rires/hmagemod/HmageMod.java
+++ src/main/java/net/rires/hmagemod/HmageMod.java
@@ -3,14 +3,17 @@ package net.rires.hmagemod;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
 import net.rires.hmagemod.machines.block.HmageFurnaceBlock;
 
 @Mod(modid = "hmagemod")
 public class HmageMod {
 
+  public static Block hmageFurnace = new HmageFurnaceBlock();
+
   @Mod.EventHandler
   public void preInit(FMLPreInitializationEvent event) {
-    GameRegistry.registerBlock(new HmageFurnaceBlock(), "hmagefurnace");
+    GameRegistry.registerBlock(hmageFurnace, "hmagefurnace");
   }
 
 }
